383305a46fc45af501dc98b00b075a00b9eec0e2
本岡 <tennteru21@gmail.com>
2019-06-21 19:16:04 +0900

    Create action

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,4 +3,33 @@ class BlogController < ApplicationController
   def index
     @blog = Blog.all.order("id DESC")
   end
+
+  def new
+    
+  end
+
+  def create
+    Blog.create(blog_params)
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  def show
+    
+  end
+  private
+  def blog_params
+    params.permit(:title, :text)
+    
+  end
 end
